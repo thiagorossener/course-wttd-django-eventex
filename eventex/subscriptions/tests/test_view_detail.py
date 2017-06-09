5f21160628013843a2a81f33a9b7f825d10bc3b7
Thiago Rossener <thiago.rossener@gmail.com>
2017-06-09 12:23:38 -0300

    Extract hardcoded urls

--- eventex/subscriptions/tests/test_view_detail.py
+++ eventex/subscriptions/tests/test_view_detail.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
 from eventex.subscriptions.models import Subscription
 
@@ -11,7 +12,7 @@ class SubscriptionDetailGet(TestCase):
             email='thiago@rossener.com',
             phone='12-981491771'
         )
-        self.response = self.client.get('/inscricao/{}/'.format(self.obj.pk))
+        self.response = self.client.get(r('subscriptions:detail', self.obj.pk))
 
     def test_get(self):
         self.assertEqual(200, self.response.status_code)
@@ -36,5 +37,5 @@ class SubscriptionDetailGet(TestCase):
 
 class SubscriptionDetailNotFound(TestCase):
     def test_not_found(self):
-        response = self.client.get('/inscricao/0/')
+        response = self.client.get(r('subscriptions:detail', 0))
         self.assertEqual(404, response.status_code)
